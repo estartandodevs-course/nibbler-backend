438a5ef9860a2f140e24cce846d6cd81969823aa
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-11-25 01:33:05 -0300

    feat(auth): Implementar funcionalidade de atualização de usuário (Development)
    Este commit adiciona a capacidade dos usuários atualizarem suas informações de perfil, incluindo nome, data de nascimento e foto de perfil. Inclui:
    
    Adicionada a classe AppSettings para gerenciar as configurações da aplicação.
    Criado o projeto Nibbler.Autenticacao.App para lógica relacionada à autenticação.
    Adicionado AtualizarUsuarioInputModel para tratar a entrada de dados na atualização do usuário.
    Implementado AtualizarUsuarioCommand para encapsular a lógica de atualização de usuário.
    Modificado UsuarioInputModel para incluir validação de campos obrigatórios.

---
+++ src/BuildingBlocks/Nibbler.WebAPI.Core/Identidade/AppSettings.cs
@@ -0,0 +1,10 @@
+namespace Nibbler.WebAPI.Core.Identidade;
+
+public class AppSettings
+{
+    public string Secret { get; set; }
+    
+    public int ExpiracaoHoras { get; set; }
+    public string Emissor { get; set; }
+    public string ValidoEm { get; set; }
+}
